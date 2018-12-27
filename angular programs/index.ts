638b43a6ae30eab60f23e84bfc11515c8dda4b6d
shubhammadankar <shubhammadankar95@gmail.com>
2018-12-27 12:41:44 +0530

    27-12-18

---
+++ angular programs/index.ts
@@ -0,0 +1,17 @@
+var factorial:number = 1;
+var num: number;
+var function1 = (a: number) => {
+
+
+   for (let i=0;i<a;i++){
+     if(i%2==0){
+      console.log("number is prime")  
+     }
+     else{
+        console.log("no is not prime")
+     }
+
+   }
+}
+
+function1(9)
